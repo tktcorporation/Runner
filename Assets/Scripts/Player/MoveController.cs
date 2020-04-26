e096cf5b302450c4b3fad9da60fde745387b2827
tkt <tktcorporation.go@gmail.com>
2020-04-26 20:07:31 +0900

    add horizontal move controller

---
+++ Assets/Scripts/Player/MoveController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController
+{
+    private Rigidbody2D rb;
+
+    public MoveController(Rigidbody2D r)
+    {
+        rb = r;
+    }
+
+    public void Move(float moveHorizontal, int powerLevel)
+    {
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+        rb.AddForce(movement * powerLevel);
+    }
+}
