d70db1df7e34d90a529b461780dc8364634a8649
tkt <tktcorporation.go@gmail.com>
2020-04-26 20:09:37 +0900

    update Octopus to use move and jump controller

---
+++ Assets/Scripts/Player/OctopusController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctopusController : MonoBehaviour
+{
+    public int powerLevel;
+    public int jumpPowerLevel;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        new MoveController(rb).Move(Input.GetAxis("Horizontal"), powerLevel);
+        new JumpController(rb, transform).Jump(Input.GetKeyDown(KeyCode.Space));
+    }
+
+    class ForceMap
+    {
+        public static float jumpForce = 22f;
+        public static float jumpThreshold = 1f;
+        public static float runForce = 1.5f;
+        public static float runSpeed = 0.5f;
+        public static float runThreshold = 2.2f;
+    }
+}
