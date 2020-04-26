07841e574de4d87e4ce611b1e9b93eb2d386dc51
tkt <tktcorporation.go@gmail.com>
2020-04-26 20:07:50 +0900

    add jump controller

---
+++ Assets/Scripts/Player/JumpController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpController
+{
+    private Rigidbody2D rb;
+    private Transform transform;
+
+    public JumpController(Rigidbody2D r, Transform t)
+    {
+        rb = r;
+        transform = t;
+    }
+
+    public void Jump(bool keydown)
+    {
+        if (!keydown)
+            return;
+        if (!isEnableToJump())
+            return;
+        AddForce();
+    }
+
+    private bool isEnableToJump()
+    {
+        if (Mathf.Abs(rb.velocity.y) > ForceMap.jumpThreshold)
+            return false;
+        return true;
+    }
+
+    private void AddForce()
+    {
+        rb.AddForce(transform.up * ForceMap.jumpForce);
+    }
+
+    class ForceMap
+    {
+        public static float jumpForce = 2000f;
+        public static float jumpThreshold = 1f;
+    }
+}
