c50b9438bafa0f29f61314d091fe79175d7694e0
tkt <tktcorporation.go@gmail.com>
2020-05-03 00:41:26 +0900

    add script to coin for rotate

---
+++ Assets/Scripts/Coin/CoinController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.angularVelocity = new Vector3(0f, 2f, 0f);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
