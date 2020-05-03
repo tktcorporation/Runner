f07bce719d854541468c13a517198c2d222746ee
tkt <tktcorporation.go@gmail.com>
2020-05-04 05:37:02 +0900

    add the running domain

---
+++ Assets/Scripts/Domain/Running/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Running
+{
+    public class Controller
+    {
+        private Player.Base player;
+        private ForceMap forceMap;
+
+        public Controller(Player.Base player, ForceMap ForceMap)
+        {
+            this.player = player;
+            forceMap = ForceMap;
+        }
+
+        public void Move(float moveHorizontal)
+        {
+            Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+            if (IsOverMaxSpeed())
+                player.rigidbody.AddForce(movement * forceMap.Force);
+            player.transform.position += new Vector3(forceMap.Speed * Time.deltaTime * moveHorizontal, 0, 0);
+        }
+
+        private bool IsOverMaxSpeed()
+        {
+            float speedX = Mathf.Abs(player.rigidbody.velocity.x);
+            if (speedX < forceMap.Threshold)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/Domain/Running/ForceMap.cs
@@ -0,0 +1,18 @@
+﻿namespace Running
+{
+    public class ForceMap
+    {
+        public float Force = 30f;
+        public float Speed = 20f;
+        public float Threshold = 17f;
+
+        public ForceMap() { }
+        public ForceMap(float force, float speed, float threshold)
+        {
+            Force = force;
+            Speed = speed;
+            Threshold = threshold;
+        }
+    }
+}
+
