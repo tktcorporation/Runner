e7e367956517deffbe7cddd01f41d43c8ade792a
tkt <tktcorporation.go@gmail.com>
2020-05-04 06:28:43 +0900

    add the domain for failing

---
+++ Assets/Scripts/Domain/Failing/Controller.cs
@@ -0,0 +1,21 @@
+﻿namespace Failing
+{
+    public class Controller
+    {
+        float failHeight = -100f;
+        Player.Base player;
+
+        public Controller(Player.Base player, float failHeight)
+        {
+            this.player = player;
+            this.failHeight = failHeight;
+        }
+
+        public bool IsFailed()
+        {
+            if (player.transform.position.y < failHeight)
+                return true;
+            return false;
+        }
+    }
+}
