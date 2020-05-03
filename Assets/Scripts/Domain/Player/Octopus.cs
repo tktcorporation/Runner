53d6d96d3f9c2574271056022190239d0f0f3142
tkt <tktcorporation.go@gmail.com>
2020-05-04 05:37:52 +0900

    add the octopus domain

---
+++ Assets/Scripts/Domain/Player/Octopus.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Player
+{
+    public class Octopus : Base
+    {
+        Running.Controller runningController;
+        Jumping.Controller jumpingController;
+
+        public Octopus(
+            Transform t,
+            Rigidbody r,
+            Running.ForceMap rForceMap,
+            Jumping.ForceMap jForceMap
+        ) : base(t,r)
+        {
+            runningController = new Running.Controller(this, rForceMap);
+            jumpingController = new Jumping.Controller(this, jForceMap);
+        }
+        public void Run(float moveHorizontal)
+        {
+            runningController.Move(moveHorizontal);
+        }
+        public void Jump(bool execute)
+        {
+            jumpingController.Jump(execute);
+        }
+    }
+}
