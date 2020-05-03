ed62c06391505014e5658eb465c2eab734639f0f
tkt <tktcorporation.go@gmail.com>
2020-05-04 05:35:50 +0900

    add player domain

---
+++ Assets/Scripts/Domain/Player/Base.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+namespace Player
+{
+    public class Base
+    {
+        public Transform transform;
+        public Rigidbody rigidbody;
+
+        public Base(Transform transform, Rigidbody rigidbody)
+        {
+            this.transform = transform;
+            this.rigidbody = rigidbody;
+        }
+        public void ChangePosition(Vector3 position)
+        {
+            transform.position = position;
+        }
+    }
+}
+
