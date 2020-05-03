9112ac5f069e762a0c529ff523373ddbd6da473d
tkt <tktcorporation.go@gmail.com>
2020-05-04 05:36:18 +0900

    add Jumping domain

---
+++ Assets/Scripts/Domain/Jumping/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jumping
+{
+    public class Controller
+    {
+        ForceMap forceMap;
+        Player.Base player;
+
+        public Controller(Player.Base player, ForceMap forceMap)
+        {
+            this.forceMap = forceMap;
+            this.player = player;
+        }
+
+        public void Jump(bool keydown)
+        {
+            if (!keydown)
+                return;
+            if (!isEnableToJump())
+                return;
+            AddForce();
+        }
+
+        private bool isEnableToJump()
+        {
+            if (Mathf.Abs(player.rigidbody.velocity.y) > forceMap.jumpThreshold)
+                return false;
+            return true;
+        }
+
+        private void AddForce()
+        {
+            player.rigidbody.AddForce(player.transform.up * forceMap.jumpForce);
+        }
+    }
+}
---
+++ Assets/Scripts/Domain/Jumping/ForceMap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Jumping
+{
+    public class ForceMap
+    {
+        public float jumpForce = 1500f;
+        public float jumpThreshold = 0.5f;
+
+        public ForceMap() { }
+        public ForceMap(float JumpForce, float JumpThreshold)
+        {
+            jumpForce = JumpForce;
+            jumpThreshold = JumpThreshold;
+        }
+    }
+}
