ab0666258ca3cd63451aa789419419f0d938f4b8
tkt <tktcorporation.go@gmail.com>
2020-05-04 09:42:08 +0900

    add a domain for points text

---
+++ Assets/Scripts/Domain/UI/PointsText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.UI;
+namespace UI
+{
+    public class PointsText
+    {
+        Text text;
+        public PointsText(Text text)
+        {
+            this.text = text;
+        }
+        public void updateText(string text)
+        {
+            this.text.text = text;
+        }
+    }
+}
