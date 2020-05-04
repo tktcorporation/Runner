332639620fbef6b0cc696aeb700a56c88f4151e2
tkt <tktcorporation.go@gmail.com>
2020-05-04 09:18:14 +0900

    add CoinsManager for generate coins

---
+++ Assets/Scripts/MonoBehaviour/Coin/CoinsManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoinsManager : MonoBehaviour
+{
+    public static string prefabPath = "Item/Coin";
+    GameObject coin;
+
+    IEnumerator Generating()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3f);
+            Generate();
+        }
+    }
+    void Generate()
+    {
+        Instantiate(coin);
+    }
+
+    // LifeCycle Methods
+    void Start()
+    {
+        coin = Item.Coin.GetPrefab(prefabPath);
+        StartCoroutine("Generating");
+    }
+
+    void Update()
+    {
+    }
+
+}
