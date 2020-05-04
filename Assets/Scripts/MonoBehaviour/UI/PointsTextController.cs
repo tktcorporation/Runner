d4a39cbe3c0459972373f2fb75b41e64d1c6907b
tkt <tktcorporation.go@gmail.com>
2020-05-04 09:43:33 +0900

    add a script of points text controller

---
+++ Assets/Scripts/MonoBehaviour/UI/PointsTextController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsTextController : MonoBehaviour
+{
+    UI.PointsText pointsText;
+    void Start()
+    {
+        pointsText = new UI.PointsText(GetComponent<Text>());
+    }
+    void LateUpdate()
+    {
+        pointsText.updateText(Manager.points.ToString());
+    }
+}
