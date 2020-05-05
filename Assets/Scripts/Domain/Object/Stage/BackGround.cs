436621a9fed96dda39c2cb0321131d9285d2c3b1
tkt <tktcorporation.go@gmail.com>
2020-05-06 03:20:19 +0900

    add a domain for background

---
+++ Assets/Scripts/Domain/Object/Stage/BackGround.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+namespace Object.Stage
+{
+    public class BackGround : Base
+    {
+        Camera camera;
+        float xMiddleWidth;
+        public BackGround(
+            SpriteRenderer sprite,
+            Camera camera,
+            int magnifications
+        ) : base(sprite.transform)
+        {
+            this.camera = camera;
+            this.xMiddleWidth = sprite.bounds.size.x / magnifications;
+        }
+        public void CheckCameraPositionAndUpdate()
+        {
+            float xPosition = transform.position.x;
+            DirectionMap direction = GetDirectionToReplace(xPosition, xMiddleWidth);
+            if (direction == DirectionMap.center)
+                return;
+            ChangePosition(
+                new Vector3(
+                    CalculateTargetPositionX(xPosition, xMiddleWidth, direction),
+                    transform.position.y,
+                    transform.position.z
+                )
+            );
+        }
+        float CalculateTargetPositionX(float xPosition, float xMiddleWidth, DirectionMap direction)
+        {
+            return xPosition + ((int)direction * xMiddleWidth);
+        }
+        DirectionMap GetDirectionToReplace(float xPosition, float xMiddleWidth)
+        {
+            float xCameraPosition = camera.transform.position.x;
+            if (xCameraPosition > CalculateTargetPositionX(xPosition, xMiddleWidth, DirectionMap.front))
+                return DirectionMap.front;
+            if (xCameraPosition < CalculateTargetPositionX(xPosition, xMiddleWidth, DirectionMap.back))
+                return DirectionMap.back;
+            return DirectionMap.center;
+        }
+    }
+    enum DirectionMap : int
+    {
+        front = 1,
+        back = -1,
+        center = 0
+    }
+}
+
