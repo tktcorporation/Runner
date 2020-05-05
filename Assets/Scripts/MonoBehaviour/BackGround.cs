024dbd898ca5fabcae93477efd88a556e5556ca5
tkt <tktcorporation.go@gmail.com>
2020-05-06 03:21:31 +0900

    update background script

--- Assets/Scripts/MonoBehaviour/BackGround.cs
+++ Assets/Scripts/MonoBehaviour/BackGround.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BackGround : MonoBehaviour
 {
@@ -10,52 +8,19 @@ public class BackGround : MonoBehaviour
     //[SerializeField]
     //int spriteCount = 2; //背景オブジェクトの横の数
 
-    float width;
-    float xMiddleWidth;
-    SpriteRenderer sprite;
-    Camera mainCamera;
+    Object.Stage.BackGround backGround;
 
-    //初期化
     private void Start()
     {
-        sprite = GetComponent<SpriteRenderer>();
-        width = sprite.bounds.size.x;
-        xMiddleWidth = width / 4;
-        mainCamera = Camera.main;
+        backGround = new Object.Stage.BackGround(
+            GetComponent<SpriteRenderer>(),
+            Camera.main,
+            4
+        );
     }
 
     private void LateUpdate()
     {
-        float xPosition = sprite.transform.position.x;
-        DirectionMap direction = getDirectionToReplace(xPosition, xMiddleWidth);
-        if (direction == DirectionMap.center)
-            return;
-        transform.position = new Vector3(
-            calculateTargetPositionX(xPosition,xMiddleWidth, direction),
-            sprite.transform.position.y,
-            sprite.transform.position.z
-        );
-    }
-
-    private float calculateTargetPositionX(float xPosition, float xMiddleWidth, DirectionMap direction)
-    {
-        return xPosition + ((int)direction * xMiddleWidth);
-    }
-
-    private DirectionMap getDirectionToReplace(float xPosition, float xMiddleWidth)
-    {
-        float xCameraPosition = mainCamera.transform.position.x;
-        if (xCameraPosition > calculateTargetPositionX(xPosition,xMiddleWidth, DirectionMap.front))
-            return DirectionMap.front;
-        if (xCameraPosition < calculateTargetPositionX(xPosition, xMiddleWidth, DirectionMap.back))
-            return DirectionMap.back;
-        return DirectionMap.center;
-    }
-
-    private enum DirectionMap : int {
-        front = 1,
-        back = -1,
-        center = 0
-
+        backGround.CheckCameraPositionAndUpdate();
     }
 }
