798c6c314d9b0d670cbfec86bae6e01f53819a3d
tkt <tktcorporation.go@gmail.com>
2020-05-06 01:30:13 +0900

    add a domain for score

---
+++ Assets/Scripts/Domain/GameSystem/Score.cs
@@ -0,0 +1,16 @@
+﻿namespace GameSystem
+{
+    public class Score
+    {
+        public int coinPoints { get; private set; } = 0;
+    public Score() { }
+        public Score(int initialCoinPoints) {
+            this.coinPoints = initialCoinPoints;
+        }
+        public int AddCoinPoints(int value)
+        {
+            this.coinPoints += value;
+            return this.coinPoints;
+        }
+    }
+}
