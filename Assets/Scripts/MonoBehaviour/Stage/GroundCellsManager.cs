a0e467f17e9c5cb362873c61821d58bc70d0632b
tkt <tktcorporation.go@gmail.com>
2020-05-06 04:47:33 +0900

    add scripts for generating infinite grounds

---
+++ Assets/Scripts/MonoBehaviour/Stage/GroundCellsManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Stage
+{
+    public class GroundCellsManager : MonoBehaviour
+    {
+        public static string prefabPath = "Stage/Ground/Cell";
+        GameObject cell;
+        public static float intervalSeconds = 0.1f;
+        public static float probabilitiesPercentage = 0.8f;
+        public static Vector3 scale = new Vector3(5,5,20);
+        public static float startPositionX = 1;
+        public static float positionY = -2.5f;
+
+        void Start()
+        {
+            cell = Object.Base.GetPrefab(prefabPath);
+            StartCoroutine(
+                Object.Stage.GroundCell.GeneratePerInteravlByProbabilities(
+                    intervalSeconds,
+                    probabilitiesPercentage,
+                    cell,
+                    scale,
+                    startPositionX,
+                    positionY
+                )
+            );
+        }
+    }
+}
+
