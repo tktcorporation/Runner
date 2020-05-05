af267064714eff0f6c4029e9f888731bde513d78
tkt <tktcorporation.go@gmail.com>
2020-05-06 03:16:39 +0900

    add Object domain

---
+++ Assets/Scripts/Domain/Object/Base.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Object
+{
+    public class Base
+    {
+        public Transform transform;
+        //Collider collider;
+        //Rigidbody rigidbody;
+
+        public Base(Transform transform)
+        {
+            this.transform = transform;
+        }
+        //public Base3D(Transform transform, Collider collider)
+        //{
+        //    this.transform = transform;
+        //    this.collider = collider;
+        //}
+        //public Base3D(Transform transform, Collider collider, Rigidbody rigidbody)
+        //{
+        //    this.transform = transform;
+        //    this.collider = collider;
+        //    this.rigidbody = rigidbody;
+        //}
+
+        public void ChangePosition(Vector3 position)
+        {
+            transform.position = position;
+        }
+    }
+}
