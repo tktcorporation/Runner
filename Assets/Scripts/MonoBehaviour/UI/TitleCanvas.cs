1855492ff7ca60f402d98e25e4f567b8d6fcf79f
tkt <tktcorporation.go@gmail.com>
2020-05-06 21:22:22 +0900

    add title canvas

---
+++ Assets/Scripts/MonoBehaviour/UI/TitleCanvas.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Scene
+{
+    public class TitleCanvas : MonoBehaviour
+    {
+        public void LoadMainScene()
+        {
+            GameSystem.Scene.BuildWithLoadScene(GameSystem.Scene.ScenesMap.Main);
+        }
+        void Start()
+        {
+            new UI.TitleCanvas(gameObject, LoadMainScene);
+        }
+    }
+}
+
