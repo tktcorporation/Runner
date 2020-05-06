e82b1192a5d14c704c56f8af7130ac373024f9f3
tkt <tktcorporation.go@gmail.com>
2020-05-06 21:05:12 +0900

    add a domain for title canvas

---
+++ Assets/Scripts/Domain/UI/TitleCanvas.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    class TitleCanvas
+    {
+        public GameObject canvas;
+
+        public TitleCanvas(GameObject canvas, UnityEngine.Events.UnityAction func)
+        {
+            this.canvas = canvas;
+            AddListenerToFirstButton(func);
+        }
+        void AddListenerToFirstButton(UnityEngine.Events.UnityAction func)
+        {
+            GetFirstButton().onClick.AddListener(func);
+        }
+        Button GetFirstButton()
+        {
+            Debug.Log(GetButtons()[0]);
+            return GetButtons()[0];
+        }
+        Button[] GetButtons()
+        {
+            return canvas.GetComponentsInChildren<Button>();
+        }
+    }
+
+}
