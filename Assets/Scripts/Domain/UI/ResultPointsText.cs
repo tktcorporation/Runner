5b27fe8c2e0e591f10e6cca4ba37cafb3a3ac723
tkt <tktcorporation.go@gmail.com>
2020-05-07 06:19:30 +0900

    add scripts for showing a result

---
+++ Assets/Scripts/Domain/UI/ResultPointsText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UI;
+namespace UI
+{
+    public class ResultPointsText
+    {
+        Text text;
+        public ResultPointsText(Text text, int points)
+        {
+            this.text = text;
+            this.text.text = CreateResultPointsText(points);
+        }
+        string CreateResultPointsText(int points)
+            => $"Result: {points}pt";
+    }
+}
---
+++ Assets/Scripts/MonoBehaviour/UI/ResultPointsController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ResultPointsController : MonoBehaviour
+{
+    void Start()
+        => new UI.ResultPointsText(gameObject.GetComponent<Text>(), Manager.score.coinPoints);
+}
