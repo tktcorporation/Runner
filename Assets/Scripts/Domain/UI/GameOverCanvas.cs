7fa8682b9a0eb1586e65a209aee7e436c2db0437
tkt <tktcorporation.go@gmail.com>
2020-05-07 06:21:37 +0900

    use lambda

--- Assets/Scripts/Domain/UI/GameOverCanvas.cs
+++ Assets/Scripts/Domain/UI/GameOverCanvas.cs
@@ -1,16 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 class GameOverCanvas
 {
     public static string Path = "UI/GameOverCanvas";
     public static GameObject GetPrefab()
-    {
-        return Resources.Load<GameObject>(Path);
-    }
+        => Resources.Load<GameObject>(Path);
 
     public GameObject obj;
 
@@ -20,15 +15,9 @@ class GameOverCanvas
         AddListenerToRetryButton(retryFunc);
     }
     void AddListenerToRetryButton(UnityEngine.Events.UnityAction func)
-    {
-        GetRetryButton().onClick.AddListener(func);
-    }
+        => GetRetryButton().onClick.AddListener(func);
     Button GetRetryButton()
-    {
-        return GetButton()[0];
-    }
+        => GetButton()[0];
     Button[] GetButton()
-    {
-        return obj.GetComponentsInChildren<Button>();
-    }
+        => obj.GetComponentsInChildren<Button>();
 }
