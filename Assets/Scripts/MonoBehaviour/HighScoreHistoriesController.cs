e9dd9e54d6ce60f7e477dbd06ed62cfef5005c0c
tkt <tktcorporation.go@gmail.com>
2020-05-07 06:11:41 +0900

    add a script for high score histories

---
+++ Assets/Scripts/MonoBehaviour/HighScoreHistoriesController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScoreHistoriesController : MonoBehaviour
+{
+    static string HighScoreHistoryCellPath = "UI/HighScoreHistoryCell";
+    static void CreateHighScoreHistories(Transform transform)
+    {
+        int i = 0;
+        GameSystem.Score.GetHighest5().ForEach(
+            (score) => {
+                GameObject obj = Instantiate(Resources.Load<GameObject>(HighScoreHistoryCellPath), transform);
+                UI.HighScoreHistoryCell cell = new UI.HighScoreHistoryCell(obj, i, score);
+                i += 1;
+            }
+        );
+    }
+    void Start()
+    {
+        CreateHighScoreHistories(transform);
+    }
+}
