976d36ab2399db26447450f1c686e7a55e562e88
tkt <tktcorporation.go@gmail.com>
2020-05-18 00:13:52 +0900

    add func for reading from firestore

--- api/repositories/repositories.go
+++ api/repositories/repositories.go
@@ -2,10 +2,12 @@ package repositories
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go"
+	"google.golang.org/api/iterator"
 )
 
 // Repository is for using firestore
@@ -23,7 +25,12 @@ type UsersRepository struct{}
 
 // Add is a func for adding
 func (arg *UsersRepository) Add(ctx context.Context, projectID string) *firestore.WriteResult {
-	return dispatchFirestore(ctx, projectID, add)
+	return dispatchFirestoreAddResult(ctx, projectID, add)
+}
+
+// Rdd is a func for reading
+func (arg *UsersRepository) Read(ctx context.Context, projectID string) ([]*firestore.DocumentSnapshot, error) {
+	return dispatchFirestoreReadResult(ctx, projectID, read)
 }
 
 func add(ctx context.Context, client firestore.Client) *firestore.WriteResult {
@@ -38,7 +45,40 @@ func add(ctx context.Context, client firestore.Client) *firestore.WriteResult {
 	return result
 }
 
-func dispatchFirestore(ctx context.Context, projectID string, fu func(ctx context.Context, client firestore.Client) *firestore.WriteResult) *firestore.WriteResult {
+func read(ctx context.Context, client *firestore.Client) ([]*firestore.DocumentSnapshot, error) {
+	iter := client.Collection("users").Documents(ctx)
+	// result := make(map[string]interface{})
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		fmt.Println(doc.Data())
+	}
+	return iter.GetAll()
+}
+
+func dispatchFirestoreAddResult(
+	ctx context.Context, projectID string,
+	fu func(ctx context.Context, client firestore.Client) *firestore.WriteResult,
+) *firestore.WriteResult {
+	client := getFirestoreClient(ctx, projectID)
+	defer client.Close()
+	return fu(ctx, *client)
+}
+func dispatchFirestoreReadResult(
+	ctx context.Context, projectID string,
+	fu func(ctx context.Context, client *firestore.Client) ([]*firestore.DocumentSnapshot, error),
+) ([]*firestore.DocumentSnapshot, error) {
+	client := getFirestoreClient(ctx, projectID)
+	defer client.Close()
+	return fu(ctx, client)
+}
+
+var getFirestoreClient = func(ctx context.Context, projectID string) *firestore.Client {
 	conf := &firebase.Config{ProjectID: projectID}
 	app, err := firebase.NewApp(ctx, conf)
 	if err != nil {
@@ -48,6 +88,5 @@ func dispatchFirestore(ctx context.Context, projectID string, fu func(ctx contex
 	if err != nil {
 		log.Fatalln(err)
 	}
-	defer client.Close()
-	return fu(ctx, *client)
+	return client
 }
